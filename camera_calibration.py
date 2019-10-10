d5c0d8553ec723d43e695522a14c66e364ea0812
Ramazan Erdem <erdemuysal@yahoo.com>
2019-10-10 09:53:47 +0200

    Class structure for camera_calibration.py
    Transforming into the class structure.

--- camera_calibration.py
+++ camera_calibration.py
@@ -1,72 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 09:28:30 2019
+
+@author: R. Erdem Uysal
+company: Upteko
+"""
+
 import numpy as np
 import cv2
 import glob
 import argparse
 from calibration_store import save_coefficients
 
+class Calibration(object):
+    def __init__(self, filePath, imgName, imgFormat, squareSize = 0.015, width = 9, height = 6):
+        self.filePath = filePath
+        self.imgName = imgName
+        self.imgFormat = imgFormat
+        self.squareSize = squareSize
+        self.width = width
+        self.height = height
+        self.criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+            
+    def calibrate(self):
+        """
+        Apply camera calibration operation for images in the given directory path.
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        """
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(8,6,0)
+        objp = np.zeros((self.height*self.width, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:self.width, 0:self.height].T.reshape(-1, 2)
+        objp = objp * self.squareSize  # Create real world coords. Use your metric.
+    
+        # Arrays to store object points and image points from all the images.
+        objpoints = []  # 3D point in real world space
+        imgpoints = []  # 2D points in image plane.
+    
+        # Directory path correction. Remove the last character if it is '/'
+        if self.filePath[-1:] == '/':
+            self.filePath = self.filePath[:-1]
+    
+        # Get the images
+        images = glob.glob(self.filePath + '/' + self.imgName + '*.' + self.imgFormat)
+        
+        # Iterate through the pairs and find chessboard corners. Add them to arrays.
+        # If openCV can't find the corners in an image, we discard the image.
+        for fileName in images:
+            img = cv2.imread(fileName)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+            # Find the chess board corners.
+            retval, corners = cv2.findChessboardCorners(gray, (self.width, self.height), None)
+    
+            # If found, add object points, image points (after refining them)
+            if retval:
+                objpoints.append(objp)
+    
+                corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), self.criteria)
+                imgpoints.append(corners2)
+    
+                # Draw and display the corners
+                # Show the image to see if pattern is found ! imshow function.
+                img = cv2.drawChessboardCorners(img, (self.width, self.height), corners2, retval)
+    
+        retval, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+        
+        return [retval, mtx, dist, rvecs, tvecs]
 
-
-def calibrate(dirpath, prefix, image_format, square_size, width=9, height=6):
-    """ Apply camera calibration operation for images in the given directory path. """
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(8,6,0)
-    objp = np.zeros((height*width, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:width, 0:height].T.reshape(-1, 2)
-
-    objp = objp * square_size  # Create real world coords. Use your metric.
-
-    # Arrays to store object points and image points from all the images.
-    objpoints = []  # 3d point in real world space
-    imgpoints = []  # 2d points in image plane.
-
-    # Directory path correction. Remove the last character if it is '/'
-    if dirpath[-1:] == '/':
-        dirpath = dirpath[:-1]
-
-    # Get the images
-    images = glob.glob(dirpath+'/' + prefix + '*.' + image_format)
-
-    # Iterate through the pairs and find chessboard corners. Add them to arrays
-    # If openCV can't find the corners in an image, we discard the image.
-    for fname in images:
-        img = cv2.imread(fname)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (width, height), None)
-
-        # If found, add object points, image points (after refining them)
-        if ret:
-            objpoints.append(objp)
-
-            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-            imgpoints.append(corners2)
-
-            # Draw and display the corners
-            # Show the image to see if pattern is found ! imshow function.
-            img = cv2.drawChessboardCorners(img, (width, height), corners2, ret)
-
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-
-    return [ret, mtx, dist, rvecs, tvecs]
+    def save_coefficients(self, path):
+        """ 
+        Save the camera matrix and the distortion coefficients to given path/file.
+        """
+        cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_WRITE)
+        cv_file.write("K", self.mtx)
+        cv_file.write("D", self.dist)
+        # Note you *release* you don't close() a FileStorage object
+        cv_file.release()
+        
+    def load_coefficients(self, path):
+        """ 
+        Loads camera matrix and distortion coefficients. 
+        """
+        # FILE_STORAGE_READ
+        cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
+    
+        # Note we also have to specify the type to retrieve other wise we only get a
+        # FileNode object back instead of a matrix
+        camera_matrix = cv_file.getNode("K").mat()
+        dist_matrix = cv_file.getNode("D").mat()
+        
+        cv_file.release()
+        return [camera_matrix, dist_matrix]
 
 
 if __name__ == '__main__':
     # Check the help parameters to understand arguments
-    parser = argparse.ArgumentParser(description='Camera calibration')
-    parser.add_argument('--image_dir', type=str, required=True, help='image directory path')
-    parser.add_argument('--image_format', type=str, required=True,  help='image format, png/jpg')
-    parser.add_argument('--prefix', type=str, required=True, help='image prefix')
-    parser.add_argument('--square_size', type=float, required=False, help='chessboard square size')
-    parser.add_argument('--width', type=int, required=False, help='chessboard width size, default is 9')
-    parser.add_argument('--height', type=int, required=False, help='chessboard height size, default is 6')
-    parser.add_argument('--save_file', type=str, required=True, help='YML file to save calibration matrices')
+    parser = argparse.ArgumentParser(description='Camera Calibration')
+    parser.add_argument('--filePath', type=str, required=True, help="Image directory path")
+    parser.add_argument('--imgName', type=str, required=True, help="Image name")
+    parser.add_argument('--imgFormat', type=str, required=True,  help="Image format, png/jpg")
+    parser.add_argument('--squareSize', type=float, required=False, help="Chessboard square size")
+    parser.add_argument('--width', type=int, required=False, help="Chessboard width size, default is 9")
+    parser.add_argument('--height', type=int, required=False, help="Chessboard height size, default is 6")
+    parser.add_argument('--saveFile', type=str, required=True, help="YML file to save calibration matrices")
 
     args = parser.parse_args()
-
-    # Call the calibraton and save as file. RMS is the error rate, it is better if rms is less than 0.2
-    ret, mtx, dist, rvecs, tvecs = calibrate(args.image_dir, args.prefix, args.image_format, args.square_size, args.width, args.height)
-    save_coefficients(mtx, dist, args.save_file)
-    print("Calibration is finished. RMS: ", ret)
+    camera = Calibration(args.filePath, args.imgName, args.imgFormat, args.squareSize, args.width, args.height)
+    
+    # Call the calibraton and save as file. RMS is the error rate, it is better if rms is less than 0.2.
+    retval, mtx, dist, rvecs, tvecs = camera.calibrate()
+    save_coefficients(mtx, dist, args.saveFile)
+    print("Calibration is successful.\n RMS: ", retval)
